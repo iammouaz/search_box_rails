0c624dc9a6951d1e6dfcd120bd6ca5304c9494ef
Mouaz El Molkey <moaz.mulki@outlook.com>
2022-12-05 16:15:39 +0300

    remove useless files

--- app/reflexes/player_search_reflex.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class PlayerSearchReflex < ApplicationReflex
-  def search(query = '')
-    players = Player.where('name LIKE ?', "%#{query}%")
-    morph '#players-list', render(partial: 'players/players', locals: { players: players })
-    morph '#reset-link', render(partial: 'players/reset_link', locals: { query: query })
-  end
-end
