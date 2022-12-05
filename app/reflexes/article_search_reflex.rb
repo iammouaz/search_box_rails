7ac5e5e62f656bb80135e25d358372f3682c8506
Mouaz El Molkey <moaz.mulki@outlook.com>
2022-12-05 21:58:21 +0300

    change some ui

--- app/reflexes/article_search_reflex.rb
+++ app/reflexes/article_search_reflex.rb
@@ -2,7 +2,7 @@
 
 class ArticleSearchReflex < ApplicationReflex
   def search(query)
-    articles = Article.where('name LIKE ?', "%#{query[:value]}%")
+    articles = Article.where('name LIKE ?', "%#{query[:value].downcase}%")
     morph '#articles-list', render(partial: 'articles/articles', locals: { articles: articles })
     morph '#reset-link', render(partial: 'articles/reset_link', locals: { query: query[:value] })
     if query[:value] != ''
@@ -11,7 +11,6 @@ class ArticleSearchReflex < ApplicationReflex
   end
 
   def hundle_tags(query)
-    @tag = Tag.where(name: query[:value])
-    Tag.create!(name: query[:value], user_id: query[:user_id])
+    Tag.create!(name: query[:value], user_id: query[:user_id].downcase)
   end
 end
